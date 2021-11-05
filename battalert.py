a02e4b942052a64368a231cc2640681c75255f14
Marcel Gerber <92036000+bncmge@users.noreply.github.com>
2021-11-05 08:37:40 -0700

    add script header

--- battalert.py
+++ battalert.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+"""
+Description
+License info
+"""
+
+__author__ = 'Marcel Gerber'
+__copyright__ = 'Copyright 2021, Linux Battery Alert'
+__credits__ = ['{credit_list}']
+__license__ = '{license}'
+__version__ = '0.0.1'
+__maintainer__ = 'Marcel Gerber'
+__email__ = 'info@ariaservices.ch'
+__status__ = 'dev'
