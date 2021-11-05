cffcf9cc3475a67ffb80fc08a4ea00b2c1b80da1
Marcel Gerber <92036000+bncmge@users.noreply.github.com>
2021-11-05 09:15:39 -0700

    added desktop notifications

--- battalert.py
+++ battalert.py
@@ -7,7 +7,8 @@ License info
 With code from: https://www.geeksforgeeks.org/python-script-to-shows-laptop-battery-percentage/
 
 Requirements:
-- psutil: sudo pip3 install psutil
+- psutil:  sudo pip3 install psutil
+- notify2: sudo pip3 install notify2
 """
 
 __author__ = 'Marcel Gerber'
@@ -22,21 +23,38 @@ __status__ = 'dev'
 # python script showing battery details
 import psutil
 
+# desktop notifications
+import notify2
+
 # function returning time in hh:mm:ss
 def convertTime(seconds):
     minutes, seconds = divmod(seconds, 60)
     hours, minutes = divmod(minutes, 60)
     return "%d:%02d:%02d" % (hours, minutes, seconds)
 
-# returns a tuple
+# notify2 vars
+n2_appname = 'Battery Alert'
+
+# get battery infos
 battery = psutil.sensors_battery()
+
+# prepare notification
+notify2.init(n2_appname)
+n2_alert = notify2.Notification(n2_appname, "No battery available.", 'notification-battery-low')
 
 if __name__ == "__main__":
     if battery == None:
-        print("No battery present on system.")
+        n2_alert.update(n2_appname, "No battery available.", 'notification-battery-low')
     else:
         print("Battery percentage : ", battery.percent)
         print("Power plugged in : ", battery.power_plugged)
-
-        # converting seconds to hh:mm:ss
         print("Battery left : ", convertTime(battery.secsleft))
+
+        if battery.percent < 20 and battery.power_plugged:
+            n2_alert.update(n2_appname, "Battery power is {}. Plug-in power or hibernate in 1 minute.".format(battery.percent), 'notification-battery-low')
+        else:
+            n2_alert.update(n2_appname, "Battery power is {}. Continue working.".format(battery.percent), 'notification-battery-low')
+
+
+    # show alert
+    n2_alert.show()
