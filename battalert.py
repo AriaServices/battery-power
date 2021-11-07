6a55ffc922ecf37d7b6fd2e057340bbf1bab5265
Marcel Gerber <9885015+chemage@users.noreply.github.com>
2021-11-07 21:42:52 +0100

    add logging

--- battalert.py
+++ battalert.py
@@ -3,11 +3,13 @@
 
 """
 Description
+Informs user of low battery and starts hibernate process before complete poweroff.
+
 License info
 With code from: https://www.geeksforgeeks.org/python-script-to-shows-laptop-battery-percentage/
 
 TODO:
-- add log
+- add log to /var/log
 - add settings file
 - add cmd line params
 - add action on low bat
@@ -30,7 +32,13 @@ __maintainer__ = 'Marcel Gerber'
 __email__ = 'info@ariaservices.ch'
 __status__ = 'dev'
 
+# for path.abspath
 import os
+
+# add logging
+import logging
+log = '/var/log/battalert.log'
+logging.basicConfig(filename=log, format="[%(asctime)s] %(name)s - %(module)s (%(process)d) - %(levelname)s - %(message)s", encoding='utf-8', level=logging.DEBUG)
 
 # python script showing battery details
 import psutil
@@ -43,7 +51,7 @@ def convertTime(seconds):
     if seconds < 0: seconds = seconds * -1
     minutes, seconds = divmod(seconds, 60)
     hours, minutes = divmod(minutes, 60)
-    return "{:02d}:{:02d}:{:02d}".format(hours, minutes)
+    return "{:02d}:{:02d}".format(hours, minutes)
 
 # icon files
 img_bat_low = os.path.abspath('bat_low.png')
@@ -59,22 +67,27 @@ notify2.init(n2_appname)
 n2_alert = notify2.Notification(n2_appname, "No battery available.", img_bat_low)
 
 if __name__ == "__main__":
+    logging.info("Running {} v. {}.".format(n2_appname, __version__))
     if battery == None:
         n2_alert.update(n2_appname, "No battery available.")
+        logging.info("There is no battery available on this system. No further action.")
     else:
         bat_percent = "{:.1f}%".format(battery.percent)
         bat_time = 'N/A'
         if battery.secsleft != -1:
             bat_time = convertTime(battery.secsleft)
-        print("Battery percentage : {}".format(bat_percent))
-        print("Power plugged in :   {}".format(battery.power_plugged))
-        print("Battery left :       {}".format(bat_time))
+        logging.debug("Battery percentage : {}".format(bat_percent))
+        logging.debug("Power plugged in :   {}".format(battery.power_plugged))
+        logging.debug("Battery left :       {}".format(bat_time))
+        # logging.info("")
 
         if battery.percent < 20.0 and not battery.power_plugged:
             n2_alert.update(n2_appname, "Battery power is {}. Plug-in power or hibernate in 1 minute.".format(bat_percent), img_bat_low)
-        # else:
-        #     n2_alert.update(n2_appname, "Battery power is {}. Continue working.".format(bat_percent))
+            # show alert
+            n2_alert.show()
+        elif battery.power_plugged:
+            logging.info("Battery is plugged in. No further action.")
+        else:
+            logging.info("Battery is discharging but enough remaining at {}. No further action.".format(bat_percent))
 
-
-    # show alert
-    n2_alert.show()
+    logging.info("{} completed.".format(n2_appname))
